53c830cae3ecb1e00c65bf74f3ddf9cc9c2865f8
Roia2529 <indiroia.ece95@g2.nctu.edu.tw>
2017-08-23 20:40:10 -0600

    Initial Commit

---
+++ RayTracingP01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracingP01
+//
+//  Created by Hsuan Lee on 8/23/17.
+//  Copyright © 2017 Hsuan Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
